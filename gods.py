638ed77234619365bdadddeeb601b85506b71d3b
InsanePrawn <insane.prawny@gmail.com>
2017-07-26 04:20:12 +0200

    basic ui works. ugh.

--- gods.py
+++ gods.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-import sys, csv
+from __future__ import print_function
+import sys, csv, random
+from copy import deepcopy
 from collections import OrderedDict
 debug = True
+
 fields = ['Greek', 'Greek Romanized', 'Roman', 'Roman Anglicized', 'Etruscan', 'Egyptian', 'Description']
 gods_dicts = []
 for row in csv.DictReader(open('godinfos.csv','r'),delimiter='\t',fieldnames=fields, skipinitialspace=True):
@@ -67,7 +70,6 @@ def print_god_info_text(god_name):
 
 
 def print_random_god():
-    import random
     god_nr = random.randint(1, len(gods_dicts))
     print('Selecting random god #%i:' % god_nr)
     print('\n'.join(generate_god_info_lines(gods_dicts[god_nr - 1])))
@@ -78,13 +80,13 @@ def print_used_names(print_annotations):
     if print_annotations:
         print('Name\tAnnotation')
     for line in used_names:
-        print(line[0].strip() + ('\t'+' '.join(line[1:]) if print_annotations else '')
+        print(line[0].strip() + ('\t'+' '.join(line[1:]) if print_annotations else ''))
 
 
 def find_available_names():
     global used_names
     global gods_dicts
-    to_check = used_names.copy()
+    to_check = deepcopy(used_names)
     for god in gods_dicts:
         taken = False
         for i, name in to_check:
@@ -101,48 +103,95 @@ def print_available_names():
         print(god['Greek Romanized'])
 
 
-def menu_pick_new_name():
-    actions = OrderedDict(
-        r=('random suggestion'
-    )
-
-
 def menu_edit_used_name_entry():
     pass
 
 def print_actions(menu_name, actions):
-    print('(%s) Available options:' % menu_name)
-    for action in actions.items():
-        print(action[0] + '\t' + action[1][0])
+    print('Menu "%s"\nAvailable actions:' % menu_name)
+    print('Key\tDescription')
+    print('---\t-----------')
+    for action_key, action_parameters in actions.items():
+        print(action_key + '\t' + action_parameters[0])
     return False
 
-def show_menu(menu_name, actions, return_val=False)
-    actions_cpy = actions.copy()
-    actions_cpy['?'] = ('show this help', print_actions, [menu_name, actions])
-    user_input = ''
+def show_menu(menu_name, actions, add_return_option=True, return_val=False):
+    if debug:
+        print('----------------------')
+    actions_cpy = deepcopy(actions)
+    actions_cpy['?'] = ('show this help', print_actions, [menu_name, actions_cpy])
+    if add_return_option:
+        actions_cpy['b'] = ('exit this menu', lambda b: b, [True])
     exit_menu = False
     # is not True to catch 'None' as well
+    print('(%s) What would you like to do? press ? to show available actions' % menu_name)
     while exit_menu is not True:
-        print('(%s) What would you like to do? press ? to show available actions' % menu_name)
-        if user_input not in actions:
-            print('(%s) unrecognised action, try again. Your input should usually be a single character followed by return' % menu_name)
+        print('(%s) > ' % menu_name, end="")
         user_input = sys.stdin.readline().strip()
-        action = actions[user_input]
-        exit_menu = action[1](*action[2])
+        if user_input not in actions_cpy:
+            print('(%s) unrecognised action, try again. Your input should usually be a single character followed by return' % menu_name)
+        else:
+            action = actions_cpy[user_input]
+            exit_menu = action[1](*action[2])
     return return_val
+
+
+def get_user_values(menu_name, values):
+    #values should be a (dummy)-prefilled OrderedDict
+    print('(%s) Please enter some information:' % menu_name)
+    for key in values.keys:
+        print('%s:' % key)
+        values[key] = sys.stdin.readline().strip()
+    print('done!')
+
+def menu_confirm_values(menu_name, values, refresh_values_func, return_val=True):
+    refresh_values_func(menu_name, values)
+    print('(%s) Please confirm the following values:' % menu_name)
+    for key, value in values:
+        print('%s: %s' % (key, value))
+    print('Are these values correct?')
+    actions = OrderedDict(
+        y=('yes', lambda b: b, [True]),
+        n = ('no, try again',menu_confirm_values,[menu_name, values, refresh_values_func, True, return_val])
+    )
+    show_menu(menu_name+'/Confirm', actions)
+    return return_val
+
+
+# menu declarations follow
+
+def get_random_suggestion(menu_name, results):
+    results.clear()
+    available = list(find_available_names())
+    god_nr = random.randint(1, len(available))
+    if debug:
+        print('Random suggestion: #%i out of %i available' % (god_nr, len(available)))
+    #print('\n'.join(generate_god_info_lines(available[god_nr - 1])))
+    for key, value in available[god_nr - 1].items():
+        results[key] = value
+
+PICK_NEW_NAME_ACTIONS = OrderedDict(
+    r=('get a random suggestion for a name', menu_confirm_values, ['random suggestion', OrderedDict(), get_random_suggestion])
+)
+
+EDIT_USED_NAME_ACTIONS = OrderedDict()
+
+
+
+# end of menu declarations
+
 
 
 
 def menu_main():
-    actions =  OrderedDict(
+    actions = OrderedDict(
         u=('Print used names', print_used_names, [False]),
-        i=('get used names + associated system info', print_used_names, [True]),
+        p=('get used names + associated system info', print_used_names, [True]),
         a=('print available names', print_available_names, []),
-        n=('pick a new name and add it to the use list', menu_pick_new_name(), []),
-        e=('edit or delete an entry in the used list', menu_edit_used_name_entry(), []),
+        n=('pick a new name and add it to the use list', show_menu, ['Pick Name', PICK_NEW_NAME_ACTIONS]),
+        e=('edit or delete an entry in the used name list',  show_menu, ['Edit Used', EDIT_USED_NAME_ACTIONS]),
         q=('quit', lambda a: a, [True])
     )
-    show_menu('main menu', actions)
+    show_menu('Main', actions, add_return_option=False)
 
 
 if __name__ == '__main__':
